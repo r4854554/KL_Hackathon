2cdea37d2e9aa49a372e3826a0b1efedcaed76c3
IDS <indoordronesystem@outlook.com>
2019-02-20 01:22:11 +0800

    Add Readme.md and Testing.md

---
+++ HDCircles.Hackathon/Services/Commands.cs
@@ -0,0 +1,12 @@
+﻿namespace HDCircles.Hackathon
+{
+    static class Commands
+    {
+        public const string MainPageLoaded = "MainPageLoaded";
+        public const string TakeOff = "TakeOff";
+        public const string Landing = "Landing";
+
+        public const string KeyDown = "KeyDown";
+        public const string KeyUp = "KeyUp";
+    }
+}
---
+++ HDCircles.Hackathon/Testing.md
@@ -0,0 +1,4 @@
+﻿Minimum tests to perform to show the code is working
+- Able to register
+- Able to see the video feed
+- Able to take off
